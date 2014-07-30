7c8233a3c9bface9b4a49b69f6c96f0bd4e5959c
zhanminj <zhangninja@gmail.com>
2014-07-29 21:11:30 -0400

    fixed the bug that the start time does not show the actual start time

--- somethingtdo/src/com/example/somethingtdo/Event.java
+++ somethingtdo/src/com/example/somethingtdo/Event.java
@@ -95,8 +95,8 @@ public class Event {
 		return convertDate(mDate);
 	}
 	
-	public Date getStartTime() {
-		return convertDate (mStartTime);
+	public String getStartTime() {
+		return mStartTime;
 	}
 	
 	public String getDescription() {
